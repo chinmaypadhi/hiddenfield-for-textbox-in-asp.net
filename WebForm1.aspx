57d0244ea334a57062e57e964831c1471ed032e3
chinmay padhi <pahichinmay1998@gmail.com>
2021-01-10 19:24:24 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="hiddenfield_for_textbox.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+    </style>
+    <script type="text/javascript">
+       var abc= function() {
+            alert(document.getElementById("HiddenField1").value);
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td>
+                        <asp:HiddenField ID="HiddenField1" runat="server" />
+                    </td>
+                    <td>enter id to find data:<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>name:</td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>city:</td>
+                    <td>
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <asp:Button ID="Button1" runat="server" Text="UPDATE" OnClick="Button1_Click" />
+                    </td>
+                    <td>
+                        <asp:Button ID="Button2" runat="server" Text="SELECT" OnClick="Button2_Click" />
+                    </td>
+                </tr>
+            </table>
+            <h1>click the button to access hiddenfield data on the clientside</h1>
+            <input type="submit" id="txt1" value="click" onclick="abc()" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+namespace hiddenfield_for_textbox
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void update(string idhid)
+        {
+            string str = ConfigurationManager.ConnectionStrings["DBCS"].ToString();
+            SqlConnection con = new SqlConnection(str);
+            SqlCommand cmd;
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+                cmd = con.CreateCommand();
+                cmd.CommandText = "update1";
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", idhid);
+                cmd.Parameters.AddWithValue("@name", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@city", TextBox2.Text);
+
+                int x = cmd.ExecuteNonQuery();
+               
+              
+                    
+                    TextBox1.Text = "";
+                    TextBox2.Text = "";
+                
+            }
+        }
+
+        protected void select(string param1)
+        {
+            string str = ConfigurationManager.ConnectionStrings["DBCS"].ToString();
+            SqlConnection con = new SqlConnection(str);
+            SqlCommand cmd;
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+                cmd = con.CreateCommand();
+                cmd.CommandText = "disp1";
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", param1);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while(rdr.Read())
+                {
+                    HiddenField1.Value = rdr["id"].ToString();
+                    TextBox1.Text = rdr["name"].ToString();
+                    TextBox2.Text = rdr["city"].ToString();
+                }
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            select(TextBox3.Text);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            update(HiddenField1.Value);
+        }
+
+
+    }
+}
